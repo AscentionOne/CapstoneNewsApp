519afc0381ccfae652a86b822643474bdf042290
KenChen <ken22885@gmail.com>
2022-08-27 17:20:29 +0800

    Change the project to use View Binding feature
    Use view binding feature.

--- app/src/main/java/com/kenchen/capstonenewsapp/MainActivity.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.kenchen.capstonenewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kenchen.capstonenewsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // using view binding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
