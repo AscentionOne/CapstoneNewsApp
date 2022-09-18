50f5526942552d792c2db80c0383e44d9fb0ce80
KenChen <ken22885@gmail.com>
2022-09-18 17:25:16 +0800

    Update RemoteResult
    Change RemoteResult.Failure to take Throwable?

--- app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteResult.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteResult.kt
@@ -1,12 +1,10 @@
 package com.kenchen.capstonenewsapp.networking
 
-import java.lang.Exception
-
 /**
  * Network response handling
  * */
 
 sealed class RemoteResult<out T> {
     data class Success<T>(val value: T) : RemoteResult<T>()
-    data class Failure(val exception: Exception) : RemoteResult<Nothing>()
+    data class Failure(val error: Throwable?) : RemoteResult<Nothing>()
 }
