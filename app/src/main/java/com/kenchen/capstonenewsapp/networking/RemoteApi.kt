f652f0a7618f0c50c105260475c8f181f9e9867c
KenChen <ken22885@gmail.com>
2022-09-18 17:38:30 +0800

    Add getTopHeadlinesByCountry api
    Add getTopHeadlinesByCountry api to RemoteApi and RemoteApiService

--- app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApi.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApi.kt
@@ -1,6 +1,42 @@
 package com.kenchen.capstonenewsapp.networking
 
+import com.kenchen.capstonenewsapp.model.Article
+import com.kenchen.capstonenewsapp.model.response.GetNewsResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 const val BASE_URL = "https://newsapi.org/v2"
+const val API_KEY = "7ac9410c8eac436d90d7b8f121b5db36"
 
 class RemoteApi(private val remoteApiService: RemoteApiService) {
+
+    /**
+     * get top headlines news by country code
+     * */
+    fun getTopHeadlinesByCountry(
+        countryCode: String,
+        OnNewsReceived: (RemoteResult<List<Article>>) -> Unit,
+    ) {
+        remoteApiService.getTopHeadlinesByCountry(countryCode, API_KEY)
+            .enqueue(object : Callback<GetNewsResponse> {
+                override fun onResponse(
+                    call: Call<GetNewsResponse>,
+                    response: Response<GetNewsResponse>,
+                ) {
+                    val getNewsResponse = response.body()
+                    if (getNewsResponse == null) {
+                        OnNewsReceived(RemoteResult.Failure(NullPointerException("No response " +
+                                "body!")))
+                    } else {
+                        OnNewsReceived(RemoteResult.Success(getNewsResponse.articles))
+                    }
+                }
+
+                override fun onFailure(call: Call<GetNewsResponse>, error: Throwable) {
+                    OnNewsReceived(RemoteResult.Failure(error))
+                }
+
+            })
+    }
 }
--- app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApiService.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApiService.kt
@@ -1,4 +1,14 @@
 package com.kenchen.capstonenewsapp.networking
 
+import com.kenchen.capstonenewsapp.model.response.GetNewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RemoteApiService {
+
+    @GET("/top-headlines")
+    fun getTopHeadlinesByCountry(@Query("country") country: String, @Query("apiKey") apiKey: String):
+            Call<GetNewsResponse>
+
 }
