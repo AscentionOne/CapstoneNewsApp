3b07dd83e26a4b07d3f0e1d5312014f56a3c1492
KenChen <ken22885@gmail.com>
2022-09-18 17:09:17 +0800

    Update data class with Json annotation for retrofit converter-moshi

--- app/src/main/java/com/kenchen/capstonenewsapp/model/Article.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/model/Article.kt
@@ -1,7 +1,7 @@
 package com.kenchen.capstonenewsapp.model
 
 import android.os.Parcelable
-import com.kenchen.capstonenewsapp.Source
+import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 /*
@@ -9,12 +9,12 @@ import kotlinx.parcelize.Parcelize
 * */
 @Parcelize
 data class Article(
-    val source: Source,
-    val author: String?,
-    val title: String,
-    val description: String? = null,
-    val url: String,
-    val urlToImage: String? = null,
-    val publishedAt: String,
-    val content: String? = null,
+    @field:Json(name = "source") val source: Source,
+    @field:Json(name = "author") val author: String?,
+    @field:Json(name = "title") val title: String,
+    @field:Json(name = "description") val description: String? = null,
+    @field:Json(name = "url") val url: String,
+    @field:Json(name = "urlToImage") val urlToImage: String? = null,
+    @field:Json(name = "publishedAt") val publishedAt: String,
+    @field:Json(name = "content") val content: String? = null,
 ) : Parcelable
