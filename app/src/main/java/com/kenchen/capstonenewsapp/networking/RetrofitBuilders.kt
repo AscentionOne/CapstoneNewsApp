451d2109db4509adeb82d9ea55b289d12e6d534a
KenChen <ken22885@gmail.com>
2022-09-19 11:37:59 +0800

    Add logging interceptor

--- app/src/main/java/com/kenchen/capstonenewsapp/networking/RetrofitBuilders.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/networking/RetrofitBuilders.kt
@@ -1,11 +1,15 @@
 package com.kenchen.capstonenewsapp.networking
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 fun buildClient(): OkHttpClient =
     OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
         .build()
 
 fun buildRetrofit(): Retrofit {
