8d2337e494ba574a9710190089c2e76ba2efc03d
KenChen <ken22885@gmail.com>
2022-09-19 16:53:03 +0800

    Update comments

--- app/src/main/java/com/kenchen/capstonenewsapp/NewsService.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/NewsService.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 import com.kenchen.capstonenewsapp.model.Article
+import com.kenchen.capstonenewsapp.model.Source
 
 interface NewsService {
 }
