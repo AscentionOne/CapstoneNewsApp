92912fffb91186ad5a4885678a2f856d93110f53
KenChen <ken22885@gmail.com>
2022-09-25 22:06:45 +0800

    Extract RemoteError from RemoteApi into NewsViewModel
    Separate the domain knowledge from remote api to keep remote api simple and can be easily replace by other remote service in the future

--- app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApi.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/networking/RemoteApi.kt
@@ -1,16 +1,6 @@
 package com.kenchen.capstonenewsapp.networking
 
-import android.util.Log
 import com.kenchen.capstonenewsapp.model.Article
-import com.kenchen.capstonenewsapp.model.response.GetNewsResponse
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import okio.IOException
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.HttpException
-import retrofit2.Response
-import kotlin.Exception
 
 const val BASE_URL = "https://newsapi.org/v2/"
 const val API_KEY = "7ac9410c8eac436d90d7b8f121b5db36"
@@ -24,42 +14,14 @@ class RemoteApi(private val remoteApiService: RemoteApiService) {
     /**
      * get top headlines news by country code with Coroutine (suspend)
      * */
-    suspend fun getTopHeadlinesByCountry(countryCode: String): RemoteResult<List<Article>> =
+    suspend fun getTopHeadlinesByCountry(countryCode: String): List<Article> =
         try {
             val data = remoteApiService.getTopHeadlinesByCountry(countryCode, API_KEY)
             val shuffledNews = data.articles.shuffled()
-            RemoteResult.Success(shuffledNews)
+            shuffledNews
 //            RemoteResult.Success(data.articles)
         } catch (error: Exception) {
-
-            Log.d("Debug", error.toString())
-            Log.d("Debug", error.message.toString())
-            RemoteResult.Failure(mapException(error))
+            throw error
         }
-
-    // map to custom remote error
-    private fun mapException(remoteException: Exception): RemoteError {
-        return when (remoteException) {
-            is IOException -> RemoteError.NoInternetException("Please check your connection")
-            is HttpException -> RemoteError.ApiException(mapHttpExceptionMessage(remoteException
-                .code().toInt()))
-            else -> RemoteError.UnexpectedException("Unexpected Error")
-        }
-    }
-
-    // map custom http exception message based on status code
-    private fun mapHttpExceptionMessage(statusCode: Int): String {
-        return if (statusCode == 400) {
-            "Bad Request"
-        } else if (statusCode == 401) {
-            "UnAuthorized"
-        } else if (statusCode == 404) {
-            "Resource not found"
-        } else if (statusCode >= 500) {
-            "Internal server error"
-        } else {
-            "Unknown error"
-        }
-    }
 
 }
--- app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsViewModel.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsViewModel.kt
@@ -1,28 +1,40 @@
 package com.kenchen.capstonenewsapp.views.news
 
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kenchen.capstonenewsapp.App.Companion.remoteApi
 import com.kenchen.capstonenewsapp.model.Article
-import com.kenchen.capstonenewsapp.networking.RemoteError
 import com.kenchen.capstonenewsapp.networking.RemoteResult
+import com.kenchen.capstonenewsapp.utils.mapException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class NewsViewModel : ViewModel() {
 
     private val _headLineNewsLiveData = MutableLiveData<RemoteResult<List<Article>>>()
     val headLineNewsLiveData: LiveData<RemoteResult<List<Article>>> = _headLineNewsLiveData
 
+    private val _newsLoadingStateLiveData = MutableLiveData<NewsLoadingState>()
+    val newsLoadingStateLiveData: LiveData<NewsLoadingState> = _newsLoadingStateLiveData
+
     fun getTopHeadlinesNewsByCountry() {
+        _newsLoadingStateLiveData.value = NewsLoadingState.LOADING
         viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val result = remoteApi.getTopHeadlinesByCountry("us")
 
-            val result = remoteApi.getTopHeadlinesByCountry("us")
+                // use postValue instead of value because
+                // we are on IO dispatcher and not on the
+                // Main (UI) thread
+                _headLineNewsLiveData.postValue(RemoteResult.Success(result))
+                _newsLoadingStateLiveData.postValue(NewsLoadingState.LOADED)
+            } catch (error: Exception) {
+                _newsLoadingStateLiveData.postValue(NewsLoadingState.ERROR)
+                _headLineNewsLiveData.postValue(RemoteResult.Failure(mapException(error)))
+            }
 
-            // use postValue instead of value because
-            // we are on IO dispatcher and not on the
-            // Main (UI) thread
-            _headLineNewsLiveData.postValue(result)
         }
 
     }
