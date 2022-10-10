26deb76f1e289453b213a701bb4f9f188e7f402f
KenChen <ken22885@gmail.com>
2022-10-10 23:31:24 +0800

    Add database, newsRepository, datastore and Gson into App

--- app/src/main/java/com/kenchen/capstonenewsapp/App.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/App.kt
@@ -1,8 +1,13 @@
 package com.kenchen.capstonenewsapp
 
 import android.app.Application
+import com.google.gson.Gson
+import com.kenchen.capstonenewsapp.database.NewsDatabase
 import com.kenchen.capstonenewsapp.networking.RemoteApi
 import com.kenchen.capstonenewsapp.networking.buildApiService
+import com.kenchen.capstonenewsapp.prefsstore.PrefsStoreImpl
+import com.kenchen.capstonenewsapp.repository.NewsRepository
+import com.kenchen.capstonenewsapp.repository.NewsRepositoryImp
 
 class App : Application() {
 
@@ -12,6 +17,25 @@ class App : Application() {
         private val apiService by lazy { buildApiService() }
 
         val remoteApi by lazy { RemoteApi(apiService) }
+
+        val gson by lazy { Gson() }
+
+        private val database: NewsDatabase by lazy {
+            NewsDatabase.buildDatabase(instance)
+        }
+
+        private val prefsStore by lazy {
+            PrefsStoreImpl(instance)
+        }
+
+        val newsRepository: NewsRepository by lazy {
+            NewsRepositoryImp(
+                database.articleDao(),
+                database.sourceDao(),
+                remoteApi,
+                prefsStore,
+            )
+        }
     }
 
     override fun onCreate() {
