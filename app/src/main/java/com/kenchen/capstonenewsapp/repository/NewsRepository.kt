f38b70c3445030754240f1f4fa623f8136832a94
KenChen <ken22885@gmail.com>
2022-10-11 21:46:50 +0800

    Create ArticleState and update NewsRepository, NewsRepositoryImpl
    1. Create a ArticleState seald class to communicate with UI layer. Utilize separation of concern
    2. Update NewsRepository and NewsRepositoryImpl to return ArticleState back to UI layer

--- app/src/main/java/com/kenchen/capstonenewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/repository/NewsRepository.kt
@@ -1,13 +1,13 @@
 package com.kenchen.capstonenewsapp.repository
 
 import com.kenchen.capstonenewsapp.model.Article
+import com.kenchen.capstonenewsapp.model.ArticleState
 import com.kenchen.capstonenewsapp.model.Source
-import com.kenchen.capstonenewsapp.networking.RemoteResult
 import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
 
-    fun getArticles(): Flow<RemoteResult<List<Article>>>
+    fun getArticles(): Flow<ArticleState>
 
     suspend fun addArticles(articles: List<Article>)
 
@@ -20,4 +20,6 @@ interface NewsRepository {
     suspend fun addSources(sources: List<Source>)
 
     suspend fun toggleDataUsage()
+
+    fun isDataUsage(): Flow<Boolean>
 }
--- app/src/main/java/com/kenchen/capstonenewsapp/repository/NewsRepositoryImp.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/repository/NewsRepositoryImp.kt
@@ -4,11 +4,14 @@ import android.util.Log
 import com.kenchen.capstonenewsapp.database.dao.ArticleDao
 import com.kenchen.capstonenewsapp.database.dao.SourceDao
 import com.kenchen.capstonenewsapp.model.Article
+import com.kenchen.capstonenewsapp.model.ArticleState
 import com.kenchen.capstonenewsapp.model.Source
+import com.kenchen.capstonenewsapp.networking.NetworkStatusChecker
 import com.kenchen.capstonenewsapp.networking.RemoteApi
-import com.kenchen.capstonenewsapp.networking.RemoteResult
 import com.kenchen.capstonenewsapp.prefsstore.PrefsStore
+import com.kenchen.capstonenewsapp.utils.mapException
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 
 class NewsRepositoryImp(
@@ -16,38 +19,54 @@ class NewsRepositoryImp(
     private val sourceDao: SourceDao,
     private val remoteApi: RemoteApi,
     private val prefsStore: PrefsStore,
+    private val networkStatusChecker: NetworkStatusChecker,
 ) : NewsRepository {
 
     companion object {
         private const val TAG = "NewsRepositoryImpl"
     }
 
-    override fun getArticles(): Flow<RemoteResult<List<Article>>> {
+    override fun getArticles(): Flow<ArticleState> {
         return flow {
-
-            // get from local Room database first
+            // can emit ArticleState.loading() here for loading state
+            // always get from local Room database first
             val newsArticles = articleDao.getArticles()
 
-            emit(RemoteResult.Success(newsArticles))
+            emit(ArticleState.Ready(newsArticles))
 
             Log.i(TAG, "Articles from local database size = ${newsArticles.size}")
 
-            // Not sure how to use the prefsStore flow value here
+            val fetchDataOnWifiOnly = prefsStore.isDataUsage().first()
 
-            try {
-                val result = remoteApi.getTopHeadlinesByCountry("us")
+            val isOnWifi = networkStatusChecker.isOnWifiConnection()
 
-                emit(RemoteResult.Success(result))
+            // only fetch the data when
+            // 1. there is wifi connection
+            // 2. unclick the has wifi on menu item, means user can use data without wifi
+            if (fetchDataOnWifiOnly.not() || isOnWifi) {
+                Log.i(TAG, "Fetching from network")
+                try {
+                    val result = remoteApi.getTopHeadlinesByCountry("us")
 
-                // if network data is not empty, add to database
-                if (result.isNotEmpty()) {
-                    articleDao.addArticles(result)
+                    emit(ArticleState.Ready(result))
+
+                    // if network data is not empty, means the source of truth
+                    // add to database
+                    if (result.isNotEmpty()) {
+                        // clear first to prevent stacking up old articles
+                        articleDao.clearArticles()
+                        articleDao.addArticles(result)
+                    }
+
+                } catch (error: Exception) {
+                    // emit partial to show that data is coming from Room database not from
+                    // remote API
+                    emit(ArticleState.Partial(newsArticles, mapException(error)))
+                    // log the error for team to inspect
+                    Log.i(TAG, "Articles from local database network error")
                 }
-
-            } catch (error: Exception) {
-//                emit(RemoteResult.Failure(mapException(error)))
-                // log the error for team to inspect
-                Log.i(TAG, "Articles from local database network error")
+            } else {
+                Log.i(TAG, "Not fetching from network")
             }
 
         }
@@ -66,4 +85,5 @@ class NewsRepositoryImp(
 
     override suspend fun toggleDataUsage() = prefsStore.toggleDataUsage()
 
+    override fun isDataUsage(): Flow<Boolean> = prefsStore.isDataUsage()
 }
