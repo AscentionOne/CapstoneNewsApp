14690db0663aef064328b17e6c562c1e08303c66
KenChen <ken22885@gmail.com>
2022-10-11 21:52:53 +0800

    Update newsViewModel
    1. Update newsViewModel to use prefsStore data from newsRepo

--- app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsViewModel.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsViewModel.kt
@@ -1,13 +1,9 @@
 package com.kenchen.capstonenewsapp.views.news
 
 import androidx.lifecycle.*
-import com.kenchen.capstonenewsapp.App.Companion.remoteApi
-import com.kenchen.capstonenewsapp.model.Article
-import com.kenchen.capstonenewsapp.networking.RemoteResult
+import com.kenchen.capstonenewsapp.model.ArticleState
 import com.kenchen.capstonenewsapp.repository.NewsRepository
-import com.kenchen.capstonenewsapp.utils.mapException
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -24,10 +20,15 @@ class NewsViewModel(
         }
     }
 
-    private val _headLineNewsLiveData = MutableLiveData<RemoteResult<List<Article>>>()
-    val headLineNewsLiveData: LiveData<RemoteResult<List<Article>>> = _headLineNewsLiveData
+    private val _headLineNewsLiveData = MutableLiveData<ArticleState>()
+    val headLineNewsLiveData: LiveData<ArticleState> = _headLineNewsLiveData
 
     init {
+        fetchArticle()
+    }
+
+    // consume the flow data from repository
+    fun fetchArticle() {
         viewModelScope.launch(Dispatchers.IO) {
             newsRepo.getArticles().onEach { articles ->
                 _headLineNewsLiveData.postValue(articles)
@@ -35,16 +36,14 @@ class NewsViewModel(
         }
     }
 
-    // get news articles liveData from Flow
-//    val headLineNewsLiveData: LiveData<RemoteResult<List<Article>>> = newsRepo
-//        .getArticles().asLiveData()
-
     fun searchArticles(search: String) {
         viewModelScope.launch(Dispatchers.IO) {
             val filteredArticle = newsRepo.searchArticles("%$search%")
-            _headLineNewsLiveData.postValue(RemoteResult.Success(filteredArticle))
+            _headLineNewsLiveData.postValue(ArticleState.Ready(filteredArticle))
         }
     }
+
+    val isDataUsageLiveData = newsRepo.isDataUsage().asLiveData()
 
     fun toggleDataUsage() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -53,54 +52,54 @@ class NewsViewModel(
 
     }
 
-    private var job: Job? = null
+//    private var job: Job? = null
+//
+//    private val _newsLoadingStateLiveData = MutableLiveData<NewsLoadingState>()
+//    val newsLoadingStateLiveData: LiveData<NewsLoadingState> = _newsLoadingStateLiveData
+//
+//    fun onActivityReady() {
+//        // prevent refetching data when orientation changes (save resource)
+//        if (_headLineNewsLiveData.value == null) {
+//            getTopHeadlinesNewsByCountry()
+//        }
+//    }
 
-    private val _newsLoadingStateLiveData = MutableLiveData<NewsLoadingState>()
-    val newsLoadingStateLiveData: LiveData<NewsLoadingState> = _newsLoadingStateLiveData
+//    fun refreshNews() {
+//        getTopHeadlinesNewsByCountry()
+//    }
 
-    fun onActivityReady() {
-        // prevent refetching data when orientation changes (save resource)
-        if (_headLineNewsLiveData.value == null) {
-            getTopHeadlinesNewsByCountry()
-        }
-    }
-
-    fun refreshNews() {
-        getTopHeadlinesNewsByCountry()
-    }
-
-    private fun getTopHeadlinesNewsByCountry() {
-        _newsLoadingStateLiveData.value = NewsLoadingState.LOADING
-        // prevent the job from running multiple time, ex: when network is bad
-        // ans user rotate the screen.
-        // _headLineNewsLiveData.value is still null so getTopHeadlinesNewsByCountry() will be
-        // execute again
-        if (job?.isActive == true)
-            return
-        job = viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val result = remoteApi.getTopHeadlinesByCountry("us")
-
-                // use postValue instead of value because
-                // we are on IO dispatcher and not on the
-                // Main (UI) thread
-                _headLineNewsLiveData.postValue(RemoteResult.Success(result))
-                _newsLoadingStateLiveData.postValue(NewsLoadingState.LOADED)
-
-                /**
-                 * option 2:
-                 * switch the coroutine context back to the Main dispatcher.
-                 * Use value instead of postValue():
-                 * withContext(Dispatchers.Main) {
-                 * _headlineNewsLIveData.value = RemoteResult.Success(result)
-                 * _newsLoadingStateLiveData.value = NewsLoadingState.LOADED
-                }
-                 * */
-            } catch (error: Exception) {
-                _newsLoadingStateLiveData.postValue(NewsLoadingState.ERROR)
-                _headLineNewsLiveData.postValue(RemoteResult.Failure(mapException(error)))
-            }
-        }
-
-    }
+//    private fun getTopHeadlinesNewsByCountry() {
+//        _newsLoadingStateLiveData.value = NewsLoadingState.LOADING
+//        // prevent the job from running multiple time, ex: when network is bad
+//        // ans user rotate the screen.
+//        // _headLineNewsLiveData.value is still null so getTopHeadlinesNewsByCountry() will be
+//        // execute again
+//        if (job?.isActive == true)
+//            return
+//        job = viewModelScope.launch(Dispatchers.IO) {
+//            try {
+//                val result = remoteApi.getTopHeadlinesByCountry("us")
+//
+//                // use postValue instead of value because
+//                // we are on IO dispatcher and not on the
+//                // Main (UI) thread
+//                _headLineNewsLiveData.postValue(RemoteResult.Success(result))
+//                _newsLoadingStateLiveData.postValue(NewsLoadingState.LOADED)
+//
+//                /**
+//                 * option 2:
+//                 * switch the coroutine context back to the Main dispatcher.
+//                 * Use value instead of postValue():
+//                 * withContext(Dispatchers.Main) {
+//                 * _headlineNewsLIveData.value = RemoteResult.Success(result)
+//                 * _newsLoadingStateLiveData.value = NewsLoadingState.LOADED
+//                }
+//                 * */
+//            } catch (error: Exception) {
+//                _newsLoadingStateLiveData.postValue(NewsLoadingState.ERROR)
+//                _headLineNewsLiveData.postValue(RemoteResult.Failure(mapException(error)))
+//            }
+//        }
+//
+//    }
 }
