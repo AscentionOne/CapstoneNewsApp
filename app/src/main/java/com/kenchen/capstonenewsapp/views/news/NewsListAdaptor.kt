293b2eae0cf4ae72179246faecf440dc2428e87d
KenChen <ken22885@gmail.com>
2022-10-11 21:50:35 +0800

    Update NewsListAdaptor
    Update NewsListAdaptor to use updateData() to make it more efficient. Prevent recreating NewsListAdaptor everytime we update articles data from MainActivity

--- app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsListAdaptor.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/views/news/NewsListAdaptor.kt
@@ -1,13 +1,23 @@
 package com.kenchen.capstonenewsapp.views.news
 
+import android.annotation.SuppressLint
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.kenchen.capstonenewsapp.model.Article
 
 class NewsListAdaptor(
-    private var articles: List<Article>,
+//    articles: List<Article>,
     val onArticleClicked: (Article) -> Unit,
 ) : RecyclerView.Adapter<NewsListViewHolder>() {
+
+    private val internalArticles = mutableListOf<Article>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateData(articles: List<Article>) {
+        internalArticles.clear()
+        internalArticles.addAll(articles)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsListViewHolder {
         val articleView = ArticleView(parent.context)
@@ -19,13 +29,15 @@ class NewsListAdaptor(
     }
 
     override fun onBindViewHolder(holder: NewsListViewHolder, position: Int) {
-        holder.bindData(articles[position])
+        holder.bindData(internalArticles[position])
         holder.itemView.setOnClickListener {
-            onArticleClicked(articles[position])
+            onArticleClicked(internalArticles[position])
         }
     }
 
     override fun getItemCount(): Int {
-        return articles.size
+        return internalArticles.size
     }
 }
+
+// DTO  ->
