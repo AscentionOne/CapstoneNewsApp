20abd496c6e2a8f7fc4c4e8ea2149668f572dda4
KenChen <ken22885@gmail.com>
2022-10-18 18:07:25 +0800

    Update ArticleDaoTest for integration test

--- app/src/androidTest/java/com/kenchen/capstonenewsapp/persistence/ArticleDaoTest.kt
+++ app/src/androidTest/java/com/kenchen/capstonenewsapp/persistence/ArticleDaoTest.kt
@@ -6,6 +6,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kenchen.capstonenewsapp.database.NewsDatabase
 import com.kenchen.capstonenewsapp.database.dao.ArticleDao
+import com.kenchen.capstonenewsapp.model.Article
+import com.kenchen.capstonenewsapp.model.Source
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertEquals
@@ -22,6 +24,17 @@ class ArticleDaoTest {
 
     private lateinit var newsDatabase: NewsDatabase
     private lateinit var articleDao: ArticleDao
+
+    private val dummyArticle = Article(
+        Source(name = "CNN"),
+        "Ken",
+        "Test",
+        "This is a test article",
+        "www.test.com",
+        "www.test.com",
+        "CNN",
+        "Show me how to test.",
+    )
 
     @Before
     fun initDatabase() {
@@ -40,11 +53,63 @@ class ArticleDaoTest {
     }
 
     @Test
-    fun `getArticlesReturnsEmptyList`() {
+    fun addArticlesToDatabase() {
+        runBlocking {
+            articleDao.addArticles(listOf(dummyArticle))
+            val articles = articleDao.getArticles()
+
+            assertEquals(1, articles.size)
+        }
+    }
+
+    @Test
+    fun getArticlesReturnsListOfArticles() {
+        runBlocking {
+            articleDao.addArticles(listOf(dummyArticle))
+            val articles = articleDao.getArticles()
+
+            assertEquals(1, articles.size)
+        }
+    }
+
+    @Test
+    fun whenDatabaseIsEmptyGetArticlesReturnsEmptyList() {
         runBlocking {
             val articles = articleDao.getArticles()
 
             assertEquals(0, articles.size)
+        }
+    }
+
+    @Test
+    fun clearArticlesFromDatabase() {
+        runBlocking {
+            articleDao.addArticles(listOf(dummyArticle))
+            articleDao.clearArticles()
+            val articles = articleDao.getArticles()
+
+            assertEquals(listOf<Article>(), articles)
+        }
+
+    }
+
+    @Test
+    fun searchArticlesFromDatabaseWhenTitleIsMatchedReturnListOfArticles() {
+        runBlocking {
+            articleDao.addArticles(listOf(dummyArticle))
+            val articles = articleDao.searchArticles("Test")
+
+            assertEquals(1, articles.size)
+        }
+    }
+
+    @Test
+    fun searchArticlesFromDatabaseWhenNoTitleIsMatchedReturnEmptyList() {
+        runBlocking {
+            articleDao.addArticles(listOf(dummyArticle))
+            val articles = articleDao.searchArticles("TestArticle")
+
+            assertEquals(listOf<Article>(), articles)
         }
     }
 }
