f2bf2760dbdac6df9fc4fbc32287a93af6cc536a
KenChen <ken22885@gmail.com>
2022-10-19 11:52:46 +0800

    Revert "Update NewsRepositoryImpTest"
    This reverts commit 7f1fd5006400f3049cf6f61851962eaace6f06c6.

--- app/src/test/java/com/kenchen/capstonenewsapp/NewsRepositoryImpTest.kt
+++ app/src/test/java/com/kenchen/capstonenewsapp/NewsRepositoryImpTest.kt
@@ -1,6 +1,5 @@
 package com.kenchen.capstonenewsapp
 
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.kenchen.capstonenewsapp.database.dao.ArticleDao
 import com.kenchen.capstonenewsapp.database.dao.SourceDao
@@ -12,9 +11,10 @@ import com.kenchen.capstonenewsapp.networking.RemoteApi
 import com.kenchen.capstonenewsapp.networking.RemoteError
 import com.kenchen.capstonenewsapp.prefsstore.PrefsStore
 import com.kenchen.capstonenewsapp.repository.NewsRepositoryImp
-import io.mockk.*
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -61,7 +61,6 @@ class NewsRepositoryImpTest {
             mockPrefsStore,
             mockNetworkStatusChecker
         )
-        mockkStatic(Log::class)
     }
 
     @get:Rule
@@ -69,129 +68,56 @@ class NewsRepositoryImpTest {
 
     @Test
     fun `When articles are searched through title return list of matched articles`() {
-        val expectedResult = 1
 
-        coEvery { mockArticleDao.searchArticles(any()) } returns listOf(dummyArticle)
-
+        coEvery { mockArticleDao.searchArticles(any()) } answers {
+            listOf(dummyArticle)
+        }
 
         runBlocking {
             val articles = newsRepository.searchArticles("test")
 
-            assertEquals(expectedResult, articles.size)
+            assertEquals(1, articles.size)
         }
     }
 
     @Test
     fun `When articles are searched through title and no articles are matched return empty list`() {
-        val expectedResult = 0
 
-        coEvery { mockArticleDao.searchArticles(any()) } returns listOf()
+        coEvery { mockArticleDao.searchArticles(any()) } answers {
+            listOf()
+        }
 
         runBlocking {
             val articles = newsRepository.searchArticles("US")
 
-            assertEquals(expectedResult, articles.size)
+            assertEquals(0, articles.size)
         }
     }
 
     @Test
-    fun `toggle data usage`() {
-        runBlocking {
-            coEvery { mockPrefsStore.toggleDataUsage() } just runs
+    fun `when articles are fetched flow of ArticleState ready with articles is returned`() {
 
-            newsRepository.toggleDataUsage()
-
-            coVerify(exactly = 1) { mockPrefsStore.toggleDataUsage() }
-        }
-    }
-
-    @Test
-    fun `is data usage should returns flow of boolean`() {
-        val expectedResult = true
-
-        runBlocking {
-            coEvery { mockPrefsStore.isDataUsage() } returns flow { emit(true) }
-
-            val actualResult = newsRepository.isDataUsage().first()
-
-            assertEquals(expectedResult, actualResult)
-        }
-    }
-
-    @Test
-    fun `articles are fetched from database first and ArticleState ready with articles is returned`() {
-        val expectedResult = ArticleState.Ready(listOf(dummyArticle))
+        val flowArticleState = newsRepository.getArticles()
 
         coEvery { mockArticleDao.getArticles() } returns listOf(dummyArticle)
 
         runBlocking {
-            val actualResult = newsRepository.getArticles().first()
-            assertEquals(expectedResult, actualResult)
+            assertEquals(ArticleState.Ready(listOf(dummyArticle)), flowArticleState.first())
         }
-    }
 
-//    @Test
-//    fun `fetch with wifi enabled, remote api success ArticleState ready is returned`() {
-//        val expectedException = Exception("error message")
-//        every { mockPrefsStore.isDataUsage() } returns flow { emit(false) }
-//        every { mockNetworkStatusChecker.isOnWifiConnection() } returns true
-//        coEvery { mockRemoteApi.getTopHeadlinesByCountry("us") } returns listOf(dummyArticle)
-//        coEvery { mockArticleDao.getArticles() } returns listOf(dummyArticle)
-//
-//        val flowArticleState = newsRepository.getArticles()
-//
-//        runBlocking {
-//            assertEquals(ArticleState.Ready(listOf(dummyArticle)),
-//                flowArticleState.drop(1).first()
-//            )
-//        }
-//    }
-
-    @Test
-    fun `fetch with wifi enabled, remote api success return ArticleState partial with articles`() {
-        coEvery { mockArticleDao.getArticles() } returns listOf(dummyArticle)
-        every { mockPrefsStore.isDataUsage() } returns flow { emit(true) }
-        every { mockNetworkStatusChecker.isOnWifiConnection() } returns true
-        coEvery { mockRemoteApi.getTopHeadlinesByCountry("us") } returns listOf(dummyArticle)
-
-        coEvery { mockArticleDao.clearArticles() } just runs
-        coEvery { mockArticleDao.addArticles(any()) } just runs
-
-        runBlocking {
-            val flowArticleState = newsRepository.getArticles()
-
-//            println(flowArticleState.take(3).toList())
-            val articleStates = flowArticleState.toList()
-            println(flowArticleState.count()) // 2
-
-            val articleStateReady = ArticleState.Ready(listOf(dummyArticle))
-
-            assertEquals(listOf(articleStateReady, articleStateReady),
-                articleStates
-            )
-
-//            assertEquals(ArticleState.Ready(listOf(dummyArticle)),
-//                flowArticleState.drop(1).first()
-//            )
-//            assertEquals(2, flowArticleState.count())
-        }
     }
 
     @Test
-    fun `fetch with wifi enabled, remote api error return ArticleState partial with articles`() {
-        val expectedException = Exception("error message")
+    fun `when articles are fetched flow of ArticleState partial with articles is returned`() {
+
+        val flowArticleState = newsRepository.getArticles()
+
         coEvery { mockArticleDao.getArticles() } returns listOf(dummyArticle)
-        every { mockPrefsStore.isDataUsage() } returns flow { emit(false) }
-        every { mockNetworkStatusChecker.isOnWifiConnection() } returns true
-        coEvery { mockRemoteApi.getTopHeadlinesByCountry(any()) } throws expectedException
 
         runBlocking {
-            val flowArticleState = newsRepository.getArticles()
-
-            assertEquals(ArticleState.Partial(listOf(dummyArticle), RemoteError
-                .UnexpectedException("Unexpected Error")),
-                flowArticleState.drop(1).first()
-            )
+            assertEquals(ArticleState.Partial(listOf(dummyArticle), RemoteError.ApiException
+                ("error")),
+                flowArticleState.first())
         }
 
     }
