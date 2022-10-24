6bf1dc328aec0aa7c3089ffa3d178b40cb6654cc
KenChen <ken22885@gmail.com>
2022-10-24 16:38:55 +0800

    Create UI instrumented test for MainActivity

---
+++ app/src/androidTest/java/com/kenchen/capstonenewsapp/ui/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package com.kenchen.capstonenewsapp.ui
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+
+@HiltAndroidTest
+class MainActivityTest {
+
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
+    // UI test
+}
