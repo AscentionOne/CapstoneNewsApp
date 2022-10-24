336a494a85630e9ac48cd50f1d38820178ff066b
KenChen <ken22885@gmail.com>
2022-10-24 16:21:25 +0800

    Create DI for PrefsStore

--- app/src/main/java/com/kenchen/capstonenewsapp/prefsstore/PrefsStoreImpl.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/prefsstore/PrefsStoreImpl.kt
@@ -7,13 +7,15 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import java.io.IOException
+import javax.inject.Inject
 
-class PrefsStoreImpl(val context: Context) : PrefsStore {
+class PrefsStoreImpl @Inject constructor (@ApplicationContext val context: Context) : PrefsStore {
 
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
