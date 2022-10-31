f0643154201de79c6fb7de7f633abe3cf733a82a
KenChen <ken22885@gmail.com>
2022-10-31 16:22:20 +0800

    Adding fade in animation when going to news detail page

--- app/src/main/java/com/kenchen/capstonenewsapp/ui/MainActivity.kt
+++ app/src/main/java/com/kenchen/capstonenewsapp/ui/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     private var isDataUsage = false
 
-    private val adapter: NewsListAdaptor = NewsListAdaptor{ article ->
+    private val adapter: NewsListAdaptor = NewsListAdaptor { article ->
         showNewsDetail(article)
     }
 
@@ -94,6 +94,7 @@ class MainActivity : AppCompatActivity() {
         val newsDetail = Intent(this, NewsDetailActivity::class.java)
         newsDetail.putExtra(INTENT_ARTICLE_KEY, article)
         startActivity(newsDetail)
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
     }
 
     // observing the life data from newsViewModel
